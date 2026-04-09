6acf409a72adaf3fd4b034180368d39490d324c9
Tejesh Sai Porandla <48875590+tejeshsai@users.noreply.github.com>
2026-04-10 01:01:49 +0530

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        vector<int> result(temperatures.size(), 0);
+        for(int i=0;i<temperatures.size();i++){
+            
+            while(!st.empty() && temperatures[i]>temperatures[st.top()]){
+                result[st.top()] = i-st.top();
+                st.pop();
+            }
+            
+            st.push(i);
+        }
+        return result;
+    }
+};
