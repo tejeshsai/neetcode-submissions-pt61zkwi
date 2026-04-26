3051f35ed64925f6b0d7d8a96f238c7c9982e7f2
Tejesh Sai Porandla <48875590+tejeshsai@users.noreply.github.com>
2026-04-26 16:59:10 +0530

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> freq;
+        vector<vector<int>> buckets(nums.size()+1);
+
+        for(int n:nums){
+            freq[n]++;
+        }
+
+        for(const auto& pair:freq){
+            buckets[pair.second].push_back(pair.first);
+        }
+        vector<int> result;
+        for(int i=buckets.size()-1;i>0;i--){
+            for(int v:buckets[i]){
+                result.push_back(v);
+                if(result.size()==k) return result;
+            }
+        }
+        return result;
+
+    }
+};
