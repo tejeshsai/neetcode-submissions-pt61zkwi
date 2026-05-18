3093733649e9d53f53a3f47c34a06f27d0fe72f5
Tejesh Sai Porandla <48875590+tejeshsai@users.noreply.github.com>
2026-05-18 18:26:42 +0530

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.cpp
@@ -0,0 +1,35 @@
+class TimeMap {
+public:
+    unordered_map<string,vector<pair<int, string>>> store;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        store[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if(store.find(key)==store.end()){
+            return "";
+        }
+        auto& values = store[key];
+        // LAST TRUE OF Valid Region
+        int lo = 0;
+        int hi = values.size()-1;
+        while(lo<hi){
+            int mid = lo+(hi-lo+1)/2; // Right Bias
+            // Valid Condition
+            if(values[mid].first == timestamp){
+                return values[mid].second;
+            }
+            if(values[mid].first < timestamp){
+                lo = mid;
+            }else{
+                hi = mid-1;
+            }
+        }
+        if(values[lo].first<=timestamp) return values[lo].second;
+        return "";
+    }
+};
