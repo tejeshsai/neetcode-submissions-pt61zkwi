df20ff6ab04b44343c54d2c3c24061be3b47688d
Tejesh Sai Porandla <48875590+tejeshsai@users.noreply.github.com>
2026-06-02 21:04:39 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
+        ListNode* dummy = new ListNode(-1);
+        ListNode* tail = dummy;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                tail->next = l1;
+                l1 = l1->next;
+            }else{
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail= tail->next;
+        }
+        if(l1) tail->next = l1;
+        else if(l2) tail->next = l2;
+        return dummy->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty()) return nullptr;
+        int interval = 1;
+        while(interval < lists.size()){
+            for(int i=0;i+interval<lists.size();i+=interval*2){
+                lists[i] = mergeTwoLists(lists[i],lists[i+interval]);
+                cout << i << i+interval;
+            }
+            interval = interval*2;
+        }
+
+        return lists[0];
+        
+    }
+};
