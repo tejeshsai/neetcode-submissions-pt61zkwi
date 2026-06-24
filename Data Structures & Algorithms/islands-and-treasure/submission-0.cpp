749bbe612106dc405fb388444fcab61907ec012e
Tejesh Sai Porandla <48875590+tejeshsai@users.noreply.github.com>
2026-06-24 23:27:11 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        vector<pair<int,int>> dirs{
+            {1,0},
+            {-1,0},
+            {0,1},
+            {0,-1}
+        };
+        
+        queue<pair<int, int>> q;
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(),false));
+
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(grid[r][c]==0){
+                    q.push({r,c});
+                }
+            }
+        }
+
+        while(!q.empty()){
+            auto co = q.front();
+            q.pop();
+
+            int row = co.first;
+            int col = co.second;
+            for(auto dr:dirs){
+                int nr = row+dr.first;
+                int nc = col+dr.second;
+                if(nr < 0 || nr>=rows|| nc <0 || nc>=cols) continue;
+                if(grid[nr][nc]==INT_MAX){
+                    grid[nr][nc] = 1+grid[row][col];
+                    q.push({nr, nc});
+                }
+            }
+        }
+        
+    }
+};
