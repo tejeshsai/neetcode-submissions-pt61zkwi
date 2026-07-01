c1a0ab73d9b78863bff60e44517c03c3c79edab2
Tejesh Sai Porandla <48875590+tejeshsai@users.noreply.github.com>
2026-07-01 13:46:53 +0530

    Add: subsets - submission-7

---
+++ Data Structures & Algorithms/subsets/submission-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> path;
+    void dfs(vector<int>& nums, int index){
+        cout << index;
+        result.push_back(path);
+
+        for(int i=index;i<nums.size();i++){
+            path.push_back(nums[i]);
+            dfs(nums, i+1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+       dfs(nums, 0); 
+       return result;
+    }
+};
