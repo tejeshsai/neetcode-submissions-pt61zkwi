52537b3a9085a5b157c92084a4099c637f146b91
Tejesh Sai Porandla <48875590+tejeshsai@users.noreply.github.com>
2026-07-08 22:10:44 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> path;
+    unordered_set<int> visit;
+    void dfs(vector<int>& nums){
+        if(path.size() == nums.size()){
+            result.push_back(path);
+        }
+        for(int i=0;i<nums.size();i++){
+            if(visit.find(i)==visit.end()){
+                path.push_back(nums[i]);
+                visit.insert(i);
+                dfs(nums);
+                path.pop_back();
+                visit.erase(i);
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        dfs(nums);
+        return result;
+    }
+};
